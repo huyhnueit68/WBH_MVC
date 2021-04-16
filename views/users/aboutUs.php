cabcdf0887c5e1c7a74fe9b602eb2a600e0811cd
phuongnm404 <nmp23141@gmail.com>
2021-04-16 22:59:37 +0700

    sua aboutUs.php

--- views/users/aboutUs.php
+++ views/users/aboutUs.php
@@ -1,82 +1,62 @@
-<div class="container">
-    <div class="row" style="margin: 0!important">
-        <div class="col-md-3"></div>
-        <div class="col-md-6" style="margin-bottom: 20px" id="info-user">
-            <div class="vc_row wpb_row vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper">
-                <div class="wpb_text_column wpb_content_element ">
-                    <div class="wpb_wrapper">
-                        <h2 class="title a-center with-subtitle"><span><span style="font-size: 18pt;">Tại Sao Nên Mua Hàng Tại Hải Triều ?</span></span></h2><span class="subtitle a-center">Chúng tôi cam kết mang lại những giá trị cao nhất cho khách hàng khi đến với Đồng Hồ Hải Triều</span>
-                    </div>
-                </div>
-            </div></div></div></div>
-            <div class="vc_row wpb_row vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper">
-            <div class="wpb_text_column wpb_content_element ">
-                <div class="wpb_wrapper">
-                    <h3 style="text-align: left;"><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/1.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/1.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">Uy Tín Hàng Đầu</span></h3>
-                </div>
-            </div>
-            <div class="wpb_text_column wpb_content_element ">
-                <div class="wpb_wrapper">
-                    <p>Với kinh nghiệm hơn 20 năm trong ngành đồng hồ, Đồng Hồ Hải Triều tự tin đem đến cho bạn những chiếc đồng hồ tốt nhất cùng trải nghiệm tuyệt vời khi mua đồng hồ tại Hải Triều.</p>
-                </div>
-            </div>
-            <div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper">
-                <div class="wpb_text_column wpb_content_element ">
-                    <div class="wpb_wrapper">
-                        <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/2.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/2.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">1 Đổi 1</span></h3>
-                    </div>
-                </div>
+<div >
+    <div style="font-size: 20px;  text-align: justify;">
+        <br>
+        <h2 style="font-size: 30px; font-weight: bold; text-align: center;">Giới thiệu chung WatchStore ?</h2>
+        WatchStore được thành lập tại Việt Nam với những mẫu mã do chính WatchStore hợp tác với các hãng đồng hồ nổi tiếngcung cấp.
+        <p style="font-weight: bold; "> TẦM NHÌN VÀ SỨ MỆNH</p>
 
-                <div class="wpb_text_column wpb_content_element ">
-                    <div class="wpb_wrapper">
-                        <p>Chế độ 1 đổi 1 trong tuần đầu tiên nếu có bất kỳ lỗi gì do nhà sản xuất.</p>
-                    </div>
-                </div>
-                </div></div></div></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper">
-            <div class="wpb_text_column wpb_content_element ">
-                <div class="wpb_wrapper">
-                    <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/3.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/3.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">Hậu Mãi Hàng Đầu</span></h3>
-                </div>
-            </div>
-            <div class="wpb_text_column wpb_content_element ">
-                <div class="wpb_wrapper">
-                    <p>Chế độ bảo hành lên đến 5 năm cho tất cả đồng hồ chính hãng mua tại Đồng Hồ Hải Triều &lt;&lt;== Đầu Tiên Và Duy Nhất Tại Việt Nam.</p>
-                </div>
-            </div>
-            <div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper">
-                <div class="wpb_text_column wpb_content_element ">
-                    <div class="wpb_wrapper">
-                        <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/4.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/4.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">Thanh Toán Dễ Dàng (COD)</span></h3>
-                    </div>
-                </div>
-                <div class="wpb_text_column wpb_content_element ">
-                    <div class="wpb_wrapper">
-                        <p>Bạn chỉ phải trả tiền khi đã nhận được hàng! Ngay Tại Nhà Bạn! Chuyển khoản trực tiếp (Cho những bạn muốn gửi quà cho bạn bè, người thân)</p>
-                    </div>
-                </div>
-                </div></div></div></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper">
-            <div class="wpb_text_column wpb_content_element ">
-                <div class="wpb_wrapper">
-                    <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/5.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/5.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">Đổi Hàng Dễ Dàng</span></h3>
-                </div>
-            </div>
-            <div class="wpb_text_column wpb_content_element ">
-                <div class="wpb_wrapper">
-                    <p>Đồng hồ sai kích cỡ? Màu sắc không hợp với bạn? Bạn mua làm quà tặng người thân nhưng người nhận không ưng ý? Đừng lo! Bạn có thể đổi hàng trong vòng 7 ngày.</p>
-                </div>
-            </div>
-            <div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper">
-                <div class="wpb_text_column wpb_content_element ">
-                    <div class="wpb_wrapper">
-                        <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/6.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/6.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">THAY PIN MIỄN PHÍ</span></h3>
-                    </div>
-                </div>
-                <div class="wpb_text_column wpb_content_element ">
-                    <div class="wpb_wrapper">
-                        <p>Thay pin miễn phí suốt đời cho tất cả các đồng hồ được Đồng Hồ Hải Triều phân phối!</p>
-                    </div>
-                </div>
-            </div></div></div></div></div></div></div></div>
-        </div>
+        <p style="margin: 10px 30px 20px 30px;"> Với mục tiêu mang đến cho khách hàng những sản phẩm  chất lượng cao, đa dạng về kiểu dáng,WatchStore luôn phấn đấu không ngừng để trở thành công ty bán lẻ tốt , cung ứng dịch vụ giá cả tốt nhất ở thị trường Việt Nam để mang đến những sản phẩm chất lượng nhất, đẹp nhất đến tay người tiêu dùng.
+            Xây dựng WatchStore trở thành thương hiệu đồng hồ  uy tín trong lòng người tiêu dùng trong nước
+            Sứ mệnh của chúng tôi là bảo vệ quyền lợi của người tiêu dùng trong nước, đẩy lùi hàng  kém chất lượng .</p>
+        <p style="font-weight: bold">TIÊU CHÍ BÁN HÀNG</p>
+        <p style="margin: 10px 30px 20px 30px;">Hoạt động theo tiêu chí lấy sự hài lòng của khách hàng làm cơ sở để phát triển nên các sản phẩm của WatchStore luôn được kiểm tra kỹ lưỡng trong suốt quá trình thực hiện từ khâu chọn cho đến khi đóng  sản phẩm để đảm bảo mang đến cho khách hàng những sản phẩm chất lượng nhất và tạo sự an tâm cho khách hàng về nguồn gốc, xuất xứ của sản phẩm.
+            Bên cạnh đó, các sản phẩm của WatchStore luôn đảm bảo mức giá cạnh tranh nhất vì chúng tôi là nhà bán lẻ  trực tiếp mà không thông qua bất kỳ trung gian nào.
+            Hơn nữa, để đảm bảo quyền lợi cho quý khách khi mua hàng, chúng tôi sẵn sàng đổi hàng hóa đã mua nếu sản phẩm bị lỗi do nhà sản xuất.</p>
+
     </div>
+    <center ">
+        <div>
+            <h2 style="font-size: 30px; font-weight: bold">Tại Sao Nên Mua Hàng Tại WatchStore ?</h2>
+            <span style="font-size: 20px;">Chúng tôi cam kết mang lại những giá trị cao nhất cho khách hàng khi đến với WatchStore</span>
+        </div>
+        <div>
+            <table style="font-size: 20px;">
+                <tr>
+                    <td style="width: 450px;margin: 10px 10px 10px 20px;">
+                        <h3 style="text-align: left;"><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/1.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/1.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">Uy Tín Hàng Đầu</span></h3>
+                        <p>Với kinh nghiệm hơn 20 năm trong ngành đồng hồ, WatchStore tự tin đem đến cho bạn những chiếc đồng hồ tốt nhất cùng trải nghiệm tuyệt vời khi mua đồng hồ tại WatchStore</p>
+                    </td>
+                    <td style="width: 450px;margin: 10px 10px 10px 20px;">
+                        <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/2.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/2.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">1 Đổi 1</span></h3>
+                        <p>Chế độ 1 đổi 1 trong tuần đầu tiên nếu có bất kỳ lỗi gì do nhà sản xuất.</p>
+                    </td>
+                    <td style="width: 450px;margin: 10px 10px 10px 20px;" >
+                        <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/3.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/3.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">Hậu Mãi Hàng Đầu</span></h3>
+                        <p>Chế độ bảo hành lên đến 5 năm cho tất cả đồng hồ chính hãng mua tại cửa hàng WatchStore &lt;&lt;== Đầu Tiên Và Duy Nhất Tại Việt Nam.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 450px;margin: 10px 10px 10px 20px;" >
+                        <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/4.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/4.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">Thanh Toán Dễ Dàng (COD)</span></h3>
+                        <p>Bạn chỉ phải trả tiền khi đã nhận được hàng! Ngay Tại Nhà Bạn! Chuyển khoản trực tiếp (Cho những bạn muốn gửi quà cho bạn bè, người thân)</p>
+
+                    </td>
+                    <td style="width: 450px;margin: 10px 10px 10px 20px;" >
+                        <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/5.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/5.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">Đổi Hàng Dễ Dàng</span></h3>
+                        <p>Đồng hồ sai kích cỡ? Màu sắc không hợp với bạn? Bạn mua làm quà tặng người thân nhưng người nhận không ưng ý? Đừng lo! Bạn có thể đổi hàng trong vòng 7 ngày.</p>
+                    </td>
+                    <td style="width: 450px;margin: 10px 10px 10px 20px;">
+                        <h3><img loading="lazy" class="alignleft lazyloaded" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/6.png" alt="" width="32" height="32" data-ll-status="loaded"><noscript><img loading="lazy" class="alignleft" src="https://cdn3.dhht.vn/wp-content/uploads/2018/09/6.png" alt="" width="32" height="32" /></noscript><span style="font-size: 14pt;">THAY PIN MIỄN PHÍ</span></h3>
+                        <p>Thay pin miễn phí suốt đời cho tất cả các đồng hồ được WatchStore phân phối!</p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </center>
 </div>
+
+
+
+
+
+
